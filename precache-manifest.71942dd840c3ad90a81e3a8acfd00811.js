97f77d70a3043e85f5fab383bbfb9175f49bed39
Alex Kornfeld <akorn3000@gmail.com>
2018-10-09 16:45:00 -0400

    Updates

---
+++ precache-manifest.71942dd840c3ad90a81e3a8acfd00811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd4b3adb57401975a06",
+    "url": "/akorn3001.github.io-quio_assessment/static/js/runtime~main.8fd4b3ad.js"
+  },
+  {
+    "revision": "e0c0da2af95b0e265d97",
+    "url": "/akorn3001.github.io-quio_assessment/static/js/main.e0c0da2a.chunk.js"
+  },
+  {
+    "revision": "f8bbb5fb5cbe4e512c58",
+    "url": "/akorn3001.github.io-quio_assessment/static/js/1.f8bbb5fb.chunk.js"
+  },
+  {
+    "revision": "e0c0da2af95b0e265d97",
+    "url": "/akorn3001.github.io-quio_assessment/static/css/main.15cd5a4d.chunk.css"
+  },
+  {
+    "revision": "f8bbb5fb5cbe4e512c58",
+    "url": "/akorn3001.github.io-quio_assessment/static/css/1.f35cfb7b.chunk.css"
+  },
+  {
+    "revision": "a18df872a1874164310d4289236b62fb",
+    "url": "/akorn3001.github.io-quio_assessment/index.html"
+  }
+];
